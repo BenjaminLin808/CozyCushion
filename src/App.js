6ae9f30703373ce4c7557465654ff2189dfcfb97
BenjaminLin808 <benjaminlin808@gmail.com>
2023-03-31 23:27:59 -0700

    Cart

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<SingleProduct />} />
-        <Route path="/products" element={<Products />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<Error />} />
       </Routes>
